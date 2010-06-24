1eed4883593e05dc397c67a121cf47aafbcf1e58
John Maguire <john.maguire@gmail.com>
2010-06-24 18:21:05 +0000

    Removed unused code.

--- tasks.py
+++ tasks.py
@@ -3,8 +3,6 @@ from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-RAIN_KEY='rain_counter'
-
 
 class Counter(db.Model):
   count = db.IntegerProperty(indexed=False, required=True)
