b4e5df4e14c4a1b313ee3f0b956a971be8053af2
John Maguire <john.maguire@gmail.com>
2010-08-10 15:34:18 +0000

    chart generation for sparkle counters

--- clementine.py
+++ clementine.py
@@ -14,8 +14,13 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 from django.utils import simplejson
 
+import colorsys
 import hmac
 import logging
+
+import pygooglechart
+from pygooglechart import StackedVerticalBarChart
+from pygooglechart import SimpleLineChart
 
 import models
 import tasks
@@ -81,23 +86,35 @@ class CountersPage(webapp.RequestHandler):
   TEMPLATE='counters.html'
   def get(self):
     counters = tasks.Counter.all().fetch(10)
-    data = [(x.key(), x.count) for x in counters]
-
-    max_count = max([x.count for x in counters])
-    chd = 't:%s' % ','.join([str(float(x.count) / max_count * 100) for x in counters])
-    chxl = '0:|%s|' % '|'.join([x.key().name() for x in counters])
-
-    url = ('http://chart.apis.google.com/chart?'
-           'cht=bvg&chs=500x400&chd=%(chd)s&chxt=x,y'
-           '&chxr=1,0,%(max)d,5&chbh=100,25,25'
-           '&chxs=0,ff0000,12,0,lt|1,0000ff,10,1,lt&chxl=%(chxl)s')
-    url = url % {'chd':chd, 'chxl':chxl, 'max':max_count}
 
     rows = [{'name':c.key().name(), 'count':c.count} for c in counters]
 
+    chart = SimpleLineChart(1000, 300)
+    for counter in counters:
+      query = counter.snapshots
+      query.order('-date')
+      snapshots = query.fetch(30)
+      counts = [s.count for s in snapshots]
+      dates = [s.date.strftime("%d/%m") for s in snapshots]
+      for i in xrange(len(counts) - 1):
+        counts[i] -= counts[i+1]
+      counts.reverse()
+      dates.reverse()
+      chart.add_data(counts[1:])
+
+    chart.set_axis_labels(pygooglechart.Axis.BOTTOM, dates[1:])
+    chart.set_axis_labels(pygooglechart.Axis.LEFT, range(0, chart.data_y_range()[1], 5))
+
+    hsv_colours = [(float(x) / 255, 1, 1) for x in range(0, 255, 255 / len(counters))]
+    rgb_colours = [colorsys.hsv_to_rgb(*x) for x in hsv_colours]
+    hex_colours = ['%02x%02x%02x' % (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)) for x in rgb_colours]
+
+    chart.set_colours(hex_colours)
+    chart.set_legend([c.key().name() for c in counters])
+
     path = os.path.join(os.path.dirname(__file__), self.TEMPLATE)
     self.response.out.write(template.render(path,
-        { 'url': url,
+        { 'url': chart.get_url(),
           'counters': rows }))
 
 
