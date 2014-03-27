dc07a174589eb8449dd7c6e35f85309eb0724380
Arnaud Bienner <arnaud.bienner@gmail.com>
2014-03-27 02:00:48 +0100

    Update redirect script to work with SoundCloud

--- data.clementine-player.org/skydrive.py
+++ data.clementine-player.org/skydrive.py
@@ -1,8 +1,14 @@
 import webapp2
+import logging
 
 class SkydrivePage(webapp2.RequestHandler):
   def get(self):
     port = self.request.get('port')
+    if not port:
+      # SoundCloud forces us to have a fixed URL (i.e. without the port parameter)
+      # However, we can have a 'state' param, which will be append to the
+      # redirect URI
+      port = self.request.get('state')
     code = self.request.get('code')
     url = 'http://localhost:%s/?code=%s' % (port, code)
     self.redirect(str(url))
