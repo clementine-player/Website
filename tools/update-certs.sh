bdf1a5691cc532bf896ef02d2592068bae78f380
John Maguire <john.maguire@gmail.com>
2016-01-11 19:12:30 +0100

    Add script for updating certs with lets encrypt

---
+++ tools/update-certs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Generates one certificate for all clementine domains and places it in
+# /etc/letsencrypt/live/clementine-player.org
+
+/root/letsencrypt/letsencrypt-auto certonly \
+	-a webroot --webroot-path /var/www/clementine-player.org \
+	--renew-by-default \
+	-d clementine-player.org \
+	-d www.clementine-player.org \
+	-d builds.clementine-player.org \
+	-d buildbot.clementine-player.org \
+	-d data.clementine-player.org \
+	-d images.clementine-player.org \
+	-d spotify.clementine-player.org
+
+/etc/init.d/apache2 restart
