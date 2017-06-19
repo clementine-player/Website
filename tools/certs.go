653750db90515bab12b9a1bdf26041b5b5e64aa8
John Maguire <john.maguire@gmail.com>
2017-06-19 11:25:32 +0100

    Add go tool for listing appengine SSL certificates

---
+++ tools/certs.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+  "flag"
+  "log"
+
+  "golang.org/x/net/context"
+  "golang.org/x/oauth2/google"
+  "google.golang.org/api/appengine/v1beta"
+)
+
+var project = flag.String("project", "clementine-data", "Google Cloud project id")
+
+func main() {
+  flag.Parse()
+
+  ctx := context.Background()
+
+  c, err := google.DefaultClient(ctx, appengine.CloudPlatformScope)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  apps, err := appengine.New(c)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  req := apps.Apps.AuthorizedCertificates.List(*project)
+  if err := req.Pages(ctx, func(page *appengine.ListAuthorizedCertificatesResponse) error {
+    for _, cert := range page.Certificates {
+      log.Printf("%s: %v\n", cert.DisplayName, cert.ExpireTime)
+    }
+    return nil
+  }); err != nil {
+    log.Fatal(err)
+  }
+}
