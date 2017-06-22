efc080feeecd42c2cb4f93578c668040d84cfda7
John Maguire <john.maguire@gmail.com>
2017-06-22 11:34:59 +1000

    gofmt -w -s

--- tools/cmd/convert_rsa.go
+++ tools/cmd/convert_rsa.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-  "flag"
-  "io/ioutil"
-  "log"
+	"flag"
+	"io/ioutil"
+	"log"
 
-  "github.com/clementine-player/Website/tools"
+	"github.com/clementine-player/Website/tools"
 )
 
 var cert = flag.String("cert", "", "Path to PKCS8 PEM encoded private key")
 var output = flag.String("out", "rsa.pem", "Path to output PKCS1 RSA private key")
 
 func main() {
-  flag.Parse()
-  data, err := ioutil.ReadFile(*cert)
-  if err != nil {
-    log.Fatal(err)
-  }
+	flag.Parse()
+	data, err := ioutil.ReadFile(*cert)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  rsa, err := convert.PKCS8ToPKCS1(data)
-  if err != nil {
-    log.Fatal("Failed to convert key: ", err)
-  }
+	rsa, err := convert.PKCS8ToPKCS1(data)
+	if err != nil {
+		log.Fatal("Failed to convert key: ", err)
+	}
 
-  err = ioutil.WriteFile(*output, rsa, 0600)
-  if err != nil {
-    log.Fatal("Failed to write key: ", err)
-  }
+	err = ioutil.WriteFile(*output, rsa, 0600)
+	if err != nil {
+		log.Fatal("Failed to write key: ", err)
+	}
 }
--- tools/cmd/upload_cert.go
+++ tools/cmd/upload_cert.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-  "crypto/x509"
-  "encoding/pem"
-  "flag"
-  "fmt"
-  "io/ioutil"
-  "log"
-  "strings"
-  "time"
+	"crypto/x509"
+	"encoding/pem"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+	"time"
 
-  "github.com/clementine-player/Website/tools"
-  "golang.org/x/net/context"
-  "golang.org/x/oauth2/google"
-  "google.golang.org/api/appengine/v1beta"
+	"github.com/clementine-player/Website/tools"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/appengine/v1beta"
 )
 
 var project = flag.String("project", "clementine-data", "Google Cloud project id")
@@ -21,105 +21,105 @@ var fullchain = flag.String("fullchain", "", "Path to PEM encoded full certifica
 var key = flag.String("key", "", "Path to PEM encoded private key")
 
 func extractBlocks(data []byte) (ret []*pem.Block) {
-  block, rest := pem.Decode(data)
-  ret = append(ret, block)
-  if rest != nil && len(rest) > 0 {
-    ret = append(ret, extractBlocks(rest)...)
-  }
-  return ret
+	block, rest := pem.Decode(data)
+	ret = append(ret, block)
+	if rest != nil && len(rest) > 0 {
+		ret = append(ret, extractBlocks(rest)...)
+	}
+	return ret
 }
 
 func extractExpiry(chain []byte) (time.Time, error) {
-  blocks := extractBlocks(chain)
-  for _, block := range blocks {
-    certs, err := x509.ParseCertificates(block.Bytes)
-    if err != nil {
-      return time.Unix(0, 0), fmt.Errorf("Failed to parse certificates")
-    }
+	blocks := extractBlocks(chain)
+	for _, block := range blocks {
+		certs, err := x509.ParseCertificates(block.Bytes)
+		if err != nil {
+			return time.Unix(0, 0), fmt.Errorf("Failed to parse certificates")
+		}
 
-    for _, cert := range certs {
-      for _, name := range cert.DNSNames {
-        if strings.Contains(name, "clementine-player.org") {
-          return cert.NotAfter, nil
-        }
-      }
-    }
-  }
-  return time.Unix(0, 0), fmt.Errorf("Could not find clementine-player.org certificate")
+		for _, cert := range certs {
+			for _, name := range cert.DNSNames {
+				if strings.Contains(name, "clementine-player.org") {
+					return cert.NotAfter, nil
+				}
+			}
+		}
+	}
+	return time.Unix(0, 0), fmt.Errorf("Could not find clementine-player.org certificate")
 }
 
 func mapDomainToCertificate(apps *appengine.APIService, project string, domain string, certId string) error {
-  mappingRequest := apps.Apps.DomainMappings.Patch(project, domain, &appengine.DomainMapping{
-    SslSettings: &appengine.SslSettings{
-      CertificateId: certId,
-    },
-  })
-  mappingRequest.UpdateMask("sslSettings.certificateId")
-  mappingResponse, err := mappingRequest.Do()
-  if err != nil {
-    return fmt.Errorf("Failed to map certificate %s for domain %s: %v", certId, domain, err)
-  }
-  log.Println(mappingResponse)
-  return nil
+	mappingRequest := apps.Apps.DomainMappings.Patch(project, domain, &appengine.DomainMapping{
+		SslSettings: &appengine.SslSettings{
+			CertificateId: certId,
+		},
+	})
+	mappingRequest.UpdateMask("sslSettings.certificateId")
+	mappingResponse, err := mappingRequest.Do()
+	if err != nil {
+		return fmt.Errorf("Failed to map certificate %s for domain %s: %v", certId, domain, err)
+	}
+	log.Println(mappingResponse)
+	return nil
 }
 
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  ctx := context.Background()
+	ctx := context.Background()
 
-  c, err := google.DefaultClient(ctx, appengine.CloudPlatformScope)
-  if err != nil {
-    log.Fatal(err)
-  }
+	c, err := google.DefaultClient(ctx, appengine.CloudPlatformScope)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  apps, err := appengine.New(c)
-  if err != nil {
-    log.Fatal(err)
-  }
+	apps, err := appengine.New(c)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  publicKey, err := ioutil.ReadFile(*fullchain)
-  if err != nil {
-    log.Fatal("Failed to read certificate chain: ", err)
-  }
+	publicKey, err := ioutil.ReadFile(*fullchain)
+	if err != nil {
+		log.Fatal("Failed to read certificate chain: ", err)
+	}
 
-  privateKey, err := ioutil.ReadFile(*key)
-  if err != nil {
-    log.Fatal("Failed to read private key: ", err)
-  }
+	privateKey, err := ioutil.ReadFile(*key)
+	if err != nil {
+		log.Fatal("Failed to read private key: ", err)
+	}
 
-  rsaKey, err := convert.PKCS8ToPKCS1(privateKey)
-  if err != nil {
-    log.Fatal("Failed to convert key to RSA PRIVATE KEY")
-  }
+	rsaKey, err := convert.PKCS8ToPKCS1(privateKey)
+	if err != nil {
+		log.Fatal("Failed to convert key to RSA PRIVATE KEY")
+	}
 
-  expiry, err := extractExpiry(publicKey)
-  if err != nil {
-    log.Fatal("Failed to extract expiry time: ", err)
-  }
+	expiry, err := extractExpiry(publicKey)
+	if err != nil {
+		log.Fatal("Failed to extract expiry time: ", err)
+	}
 
-  cert := &appengine.AuthorizedCertificate{
-    CertificateRawData: &appengine.CertificateRawData{
-      PublicCertificate: fmt.Sprintf("%s", publicKey),
-      PrivateKey:        fmt.Sprintf("%s", rsaKey),
-    },
-    DisplayName: fmt.Sprintf("clementine-%02d-%d", expiry.Month(), expiry.Year()),
-  }
+	cert := &appengine.AuthorizedCertificate{
+		CertificateRawData: &appengine.CertificateRawData{
+			PublicCertificate: fmt.Sprintf("%s", publicKey),
+			PrivateKey:        fmt.Sprintf("%s", rsaKey),
+		},
+		DisplayName: fmt.Sprintf("clementine-%02d-%d", expiry.Month(), expiry.Year()),
+	}
 
-  req := apps.Apps.AuthorizedCertificates.Create(*project, cert)
-  resp, err := req.Do()
-  if err != nil {
-    log.Fatal("Uploading certificate failed: ", err)
-  }
+	req := apps.Apps.AuthorizedCertificates.Create(*project, cert)
+	resp, err := req.Do()
+	if err != nil {
+		log.Fatal("Uploading certificate failed: ", err)
+	}
 
-  certId := resp.Id
-  if err = mapDomainToCertificate(apps, "clementine-data", "data.clementine-player.org", certId); err != nil {
-    log.Print(err)
-  }
-  if err = mapDomainToCertificate(apps, "clementine-web", "clementine-player.org", certId); err != nil {
-    log.Print(err)
-  }
-  if err = mapDomainToCertificate(apps, "clementine-web", "www.clementine-player.org", certId); err != nil {
-    log.Print(err)
-  }
+	certId := resp.Id
+	if err = mapDomainToCertificate(apps, "clementine-data", "data.clementine-player.org", certId); err != nil {
+		log.Print(err)
+	}
+	if err = mapDomainToCertificate(apps, "clementine-web", "clementine-player.org", certId); err != nil {
+		log.Print(err)
+	}
+	if err = mapDomainToCertificate(apps, "clementine-web", "www.clementine-player.org", certId); err != nil {
+		log.Print(err)
+	}
 }
