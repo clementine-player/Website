b4cc25f1a34dbd73116db3ea4e1b269bf2ae0e69
John Maguire <john.maguire@gmail.com>
2017-07-27 16:10:33 +0100

    Remove AppEngine from other cert cron job

--- tools/update-certs.sh
+++ tools/update-certs.sh
@@ -7,17 +7,9 @@
 	-a webroot --webroot-path /var/www/clementine-player.org \
 	-q \
 	--renew-by-default \
-	-d clementine-player.org \
-	-d www.clementine-player.org \
 	-d builds.clementine-player.org \
 	-d buildbot.clementine-player.org \
-	-d data.clementine-player.org \
 	-d images.clementine-player.org \
 	-d spotify.clementine-player.org
 
 /etc/init.d/apache2 restart
-
-GOOGLE_APPLICATION_CREDENTIALS=$HOME/credentials.json \
-  go run $GOPATH/src/github.com/clementine-player/Website/tools/cmd/upload_cert.go \
-  -fullchain /etc/letsencrypt/live/clementine-player.org/fullchain.pem \
-  -key /etc/letsencrypt/live/clementine-player.org/privkey.pem
